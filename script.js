445afbc88b830c9e2f0820ee1cce51676efae19e
Jakob <99341221+jasunk@users.noreply.github.com>
2022-11-07 15:09:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let dino = document.getElementById("dino")
+let block = document.getElementById("block")
+let speed = 2
+let score = 0
+let hasGottenScore = false
+
+function jump(){
+    if (dino.classList != "jumping"){
+        dino.classList.add("jumping")
+        setTimeout(jumpstop, 650)
+    }
+}
+
+function jumpstop(){
+    dino.classList.remove("jumping")
+}
+
+setInterval(check,10)
+
+function check(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    let dinoLeft = parseInt(window.getComputedStyle(dino).getPropertyValue("left"))
+    
+    if (dinoTop>=150 && blockLeft<=75 && dinoLeft>=25) {
+        block.style.display= "none"
+        block.style.animation = "none"
+        document.body.innerHTML = "DU TAPTE BRODER, SCORE:  " + score
+    }else if(blockLeft<=75 && dinoLeft>=25 && !hasGottenScore){
+        score +=1
+        hasGottenScore=true
+        setTimeout(allowScore, 300)
+    }
+
+    document.getElementById("score").innerHTML = score
+    if (speed>0.5){
+        speed -= 0.0002
+    }
+    
+    
+    block.style.animation =  "block "+ speed +"s linear infinite"
+}
+
+function allowScore(){
+    hasGottenScore=false
+    let nr = Math.random() < 0.5
+        if (nr){
+            document.getElementById("imageid").src="henrik.jpeg";
+        }else{
+            document.getElementById("imageid").src="imran.png";
+        }
+}
